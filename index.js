6608c7a1d6a01811db017c3387611d60b5553e9a
Thanh Ramen~ <thanhramen439@gmail.com~>
2024-01-20 01:26:04 +0700

    change img link

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use('/images', express.static('upload/images'));
 app.post('/upload', upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://famstorebackend.vercel.app/images/${req.file.filename}`,
+    image_url: `https://famstorebackend.onrender.com/images/${req.file.filename}`,
   });
 });
 
