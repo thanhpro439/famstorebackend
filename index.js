3f64211b04c8c070ffdd0deafa7ad410c67e7639
Thanh Ramen~ <thanhramen439@gmail.com~>
2024-01-20 00:04:02 +0700

    add product schema

--- index.js
+++ index.js
@@ -119,41 +119,41 @@ mongoose.connect(
 //   }
 // });
 
-// // Schema for creating new product
-// const Product = mongoose.model('Product', {
-//   id: {
-//     type: Number,
-//     required: true,
-//   },
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: String,
-//     required: true,
-//   },
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   new_price: {
-//     type: Number,
-//     required: true,
-//   },
-//   old_price: {
-//     type: Number,
-//     required: true,
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   available: {
-//     type: Boolean,
-//     default: true,
-//   },
-// });
+// Schema for creating new product
+const Product = mongoose.model('Product', {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 // // Creating API for adding new product endpoint
 // app.post('/addproduct', async (req, res) => {
