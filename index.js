0c31f1ab8edb044acde957e90c050aa69460f32d
Thanh Ramen~ <thanhramen439@gmail.com~>
2024-01-20 00:18:43 +0700

    add upload func

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ mongoose.connect(
   `mongodb+srv://${env.DB_USER}:${env.DB_PASSWORD}@famstore.bc1gtb2.mongodb.net/famstore`
 );
 
-// // Image storage engine
-// const storage = multer.diskStorage({
-//   destination: './upload/images',
-//   filename: (req, file, cb) => {
-//     return cb(
-//       null,
-//       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-//     );
-//   },
-// });
+// Image storage engine
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 // // Create upload endpoint for images
 // app.use('/images', express.static('upload/images'));
